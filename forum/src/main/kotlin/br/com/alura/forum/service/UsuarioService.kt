564d75da56ea73edae53b231ce6f24a6801d9127
Alexandre Antonelli Cardoso <aacds@msn.com>
2023-06-30 09:50:46 -0300

    feat: First commit

---
+++ forum/src/main/kotlin/br/com/alura/forum/service/UsuarioService.kt
@@ -0,0 +1,21 @@
+package br.com.alura.forum.service
+
+import br.com.alura.forum.model.Usuario
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class UsuarioService(var usuarios: List<Usuario>) {
+
+    init {
+        val usuario = Usuario(
+            id = 1,
+            nome = "Ana da Silva",
+            email = "ana@email.com"
+        )
+        usuarios = Arrays.asList(usuario)
+    }
+
+    fun buscarPorId(idAutor: Long): Usuario  = usuarios.stream().filter { u -> u.id == idAutor }.findFirst().get()
+
+}
