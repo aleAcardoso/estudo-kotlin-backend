26267c81e47d6f4bf7e30c73babee0c673d0799c
Alexandre Antonelli Cardoso <aacds@msn.com>
2023-07-13 10:36:16 -0300

    feat: Tetes manuais

--- forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -12,6 +12,7 @@ import jakarta.persistence.EntityManager
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import java.util.stream.Collectors
 
 @Service
@@ -52,6 +53,7 @@ class TopicoService(
         val topico = repository.findById(form.id).orElseThrow{NotFoundException(notFoundMessage)}
         topico.titulo = form.titulo
         topico.mensagem = form.mensagem
+        topico.dataAlteracao = LocalDate.now()
         return topicoViewMapper.map(topico)
     }
 
